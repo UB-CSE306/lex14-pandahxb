0a0ac95a5ae5b8f89032bab1f2c7883eb36ffa5f
Kaifeng Li <kli73@buffalo.edu>
2025-10-09 17:00:21 -0400

    test: 8 more test cases
    3 test cases failed. Gcov shows that there's an unreachable line.

--- schemeValidatorTests.c
+++ schemeValidatorTests.c
@@ -10,6 +10,14 @@ void runtest(char *input, int expected);
 // Tests
 
 Test(coverage, test00) {  runtest("4",-1);  }
+Test(coverage, test01) {  runtest("http",0);  }
+Test(coverage, test02) {  runtest("HTTP",1);  }
+Test(coverage, test03) {  runtest("HTTP+-",1);  }
+Test(coverage, test04) {  runtest("HT.TP",1);  }
+Test(coverage, test05) {  runtest("HT*TP",-1);  }
+Test(coverage, test06) {  runtest("ht.TP",1);  }
+Test(coverage, test07) {  runtest("ht*TP",-1);  }
+Test(coverage, test08) {  cr_assert_eq(isSpecial('='), 1);  }
 
 void runtest(char *input, int expected) {
   int actual = schemeValidator(input);
